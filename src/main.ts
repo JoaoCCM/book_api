a27279975c3a09d9bed7bb1afecd5217011c6e5f
JoaoCCM <joaocarloscm7@gmail.com>
2021-06-18 20:05:58 -0300

    feat: swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
 import * as compression from 'compression';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Book API')
+    .setDescription(`REST API for book's project`)
+    .setVersion('1.0')
+    .addTag('Book API')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options, {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  });
+
+  SwaggerModule.setup('api-docs', app, document);
 
   app.use(compression());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
