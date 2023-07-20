49ab3bef62167165c4c8c944029b738156266078
Beatriz Miranda <ext.beatriz.silva@farfetch.com>
2023-07-20 15:30:18 -0300

    code adjusts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,22 +1,29 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-require('dotenv').config()
+require('dotenv').config();
 
-const { DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, NODE_ENV = null, DB_NAME } = process.env;
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PORT,
+  DB_PASSWORD,
+  NODE_ENV = null,
+  DB_NAME,
+} = process.env;
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: DB_HOST,
-    port: parseInt(DB_PORT),
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    autoLoadEntities: true,
-    entities: [__dirname + '/../entities/*.entity.{js,ts}'],
-    synchronize: NODE_ENV && NODE_ENV !== 'production' ? true : false,
-    ssl: false,
-    migrations: ['src/migrations/*.ts'],
-    migrationsTableName: 'migration',
-    cli: {
-        migrationsDir: 'src/migration',
-    },
-}
+  type: 'postgres',
+  host: DB_HOST,
+  port: parseInt(DB_PORT),
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  autoLoadEntities: true,
+  entities: [__dirname + '/../entities/*.entity.{js,ts}'],
+  synchronize: NODE_ENV && NODE_ENV !== 'production' ? true : false,
+  ssl: false,
+  migrations: ['src/migrations/*.ts'],
+  migrationsTableName: 'migration',
+  cli: {
+    migrationsDir: 'src/migration',
+  },
+};
